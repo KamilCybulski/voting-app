dc4260f82971275382f4c527e138e16e2a9162e1
Kamyl <kamillcybulski@gmail.com>
2017-08-30 20:44:02 +0200

    Added material-ui and basic navbar implementation

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,12 +1,23 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
+import AppBar from 'material-ui/AppBar';
+import FlatButton from 'material-ui/FlatButton';
+
+const GuestNavButtons = () => (
+  <div className="navButtons">
+    <Link to="/"><FlatButton label="Home" /></Link>
+    <Link to="/authentication"><FlatButton label="Login / Signup" /></Link>
+  </div>
+);
+
 
 const Nav = () => (
-  <div>
-    <Link to="/">Home</Link>
-    <Link to="/authentication">Login / Signup</Link>
-  </div>
+  <AppBar
+    title="Pointless Polls"
+    showMenuIconButton={false}
+    iconElementRight={<GuestNavButtons />}
+  />
 );
 Nav.propTypes = {
 };
