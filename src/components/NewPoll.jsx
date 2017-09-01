78f44846c249afcf254bf806bcb82d872a81fea0
Kamyl <kamillcybulski@gmail.com>
2017-09-02 00:04:55 +0200

    Added saving new polls to the DB

--- src/components/NewPoll.jsx
+++ src/components/NewPoll.jsx
@@ -2,17 +2,24 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import firebase from 'firebase';
 import TextField from 'material-ui/TextField';
+import RaisedButton from 'material-ui/RaisedButton';
 
-const Option = ({ index, handleChange, text }) => (
+
+const Option = ({ index, handleOptionsChange, text }) => (
   <div>
     <TextField
       hintText="option"
       value={text}
-      onChange={e => handleChange(index, e)}
+      onChange={e => handleOptionsChange(index, e)}
     />
   </div>
 );
 
+Option.propTypes = {
+  index: PropTypes.number.isRequired,
+  handleOptionsChange: PropTypes.func.isRequired,
+  text: PropTypes.string.isRequired,
+};
 
 class NewPoll extends React.Component {
   /**
@@ -25,7 +32,7 @@ class NewPoll extends React.Component {
     super(props);
     this.state = {
       newPollName: '',
-      newPollOptions: ['Kirk', 'Piccard', "Maui"],
+      newPollOptions: ['', ''],
     };
   }
 
@@ -35,29 +42,68 @@ class NewPoll extends React.Component {
    * @returns {Promise} Contains void;
    */
   saveToDB = () => {
-    const data = {
-      name: 'Kirk vs Piccard',
-      owner: this.props.user.email,
-      options: [
-        {
-          name: 'Piccard',
-          votes: 10,
-        },
-        {
-          name: 'Kirk',
-          votes: 15,
-        },
-      ],
-    };
+    const options = this.state.newPollOptions.map(item => ({
+      name: item,
+      votes: 0,
+    }));
 
+    const data = {
+      name: this.state.newPollName,
+      owner: this.props.user.email,
+      options,
+    };
     const newPoll = firebase.database().ref('/polls').push().key;
     return firebase.database().ref().update({ [`/polls/${newPoll}`]: data });
   }
 
-  handleChange = (index, e) => {
+  /**
+   * resetState
+   * sets the state back to initial value
+   * @returns {undefined}
+   */
+  resetState = () => {
+    this.setState({
+      newPollName: '',
+      newPollOptions: ['', ''],
+    });
+  }
+
+  /**
+   * handleOptionsChange
+   * Synchronize given item value in newPollOptions with corresponding TextField
+   * @param {number} index Index of an item that should be altered;
+   * @param {object} e Keyboard event object;
+   * @returns {undefined}
+   */
+  handleOptionsChange = (index, e) => {
     const options = this.state.newPollOptions.slice();
     options[index] = e.target.value;
     this.setState({ newPollOptions: options });
+  }
+
+  /**
+   * handleNameChange
+   * Synchronize value of newPollName with corresponding TextField;
+   * @param {object} e Keyboard event object;
+   * @returns {undefined}
+   */
+  handleNameChange = (e) => {
+    this.setState({
+      newPollName: e.target.value,
+    });
+  }
+
+  /**
+   * addOption
+   * Adds anoter option to the poll;
+   * @returns {undefined}
+   */
+  addOption = () => {
+    const options = this.state.newPollOptions.concat(['']);
+
+    this.setState({
+      newPollOptions: options,
+    });
   }
 
   /**
@@ -67,21 +113,62 @@ class NewPoll extends React.Component {
     const options = this.state.newPollOptions;
 
     return (
-      <div className="fullscreen flex-column">
-        {options.map((item, index) => (
-          <Option
-            index={index}
-            text={item}
-            handleChange={this.handleChange}
+      <div className="full-width flex-column margin-top-50">
+        <h1 className="text-big text-bold">
+          New poll
+        </h1>
+        <div className="margin-top-50">
+          <TextField
+            hintText="poll's title"
+            value={this.state.newPollName}
+            onChange={this.handleNameChange}
           />
-        ))}
+        </div>
+
+        <RaisedButton
+          className="width200 margin20"
+          primary
+          label="Add option"
+          onTouchTap={this.addOption}
+        />
+
+        <div className="margin-top-50 margin-bot-50">
+          {options.map((item, index) => (
+            <Option
+              key={index}
+              index={index}
+              hintText="option"
+              text={item}
+              handleOptionsChange={this.handleOptionsChange}
+            />
+          ))}
+        </div>
+
+        <div className="width300 flex-column margin-bot-50">
+          <RaisedButton
+            className="width200 margin20"
+            label="Reset"
+            primary
+            onTouchTap={this.resetState}
+          />
+          <RaisedButton
+            className="width200"
+            label="submit"
+            primary
+            onTouchTap={this.saveToDB}
+          />
+        </div>
       </div>
     );
   }
 }
 
+NewPoll.defaultProps = {
+  user: undefined,
+};
+
 NewPoll.propTypes = {
-  user: PropTypes.shape({ email: PropTypes.string.isRequired }).isRequired,
+  user: PropTypes.shape({ email: PropTypes.string.isRequired }),
 };
 
 export default NewPoll;
