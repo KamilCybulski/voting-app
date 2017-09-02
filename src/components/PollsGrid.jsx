083f758f523d0cd0cb8c4f28e4622b4f298c4a8d
Kamyl <kamillcybulski@gmail.com>
2017-09-02 19:31:42 +0200

    Style improvements in PollsGrid

--- src/components/PollsGrid.jsx
+++ src/components/PollsGrid.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 import { List, ListItem } from 'material-ui/List';
 import LensIcon from 'material-ui/svg-icons/image/lens';
 import Divider from 'material-ui/Divider';
@@ -10,18 +11,20 @@ import Loader from '../utils/Loader';
 function PollsGrid({ polls }) {
   return polls === undefined
     ? <Loader />
-    : <div className="center-items margin-top-50">
+    : <div className="center-items margin-top-50 margin-bot-50">
       <h1 className="text-big text-bold text-center line-height-std">
         Check out all these pointless polls!
       </h1>
       <List className="margin-top-50">
         {Object.keys(polls).map(key => (
           <div key={key}>
-            <ListItem
-              primaryText={polls[key].name}
-              secondaryText={`made by: ${polls[key].owner}`}
-              leftIcon={<LensIcon color={cyan500} />}
-            />
+            <Link to={`/poll/${key}`} className="no-text-decor" >
+              <ListItem
+                primaryText={polls[key].name}
+                secondaryText={`made by: ${polls[key].owner}`}
+                leftIcon={<LensIcon color={cyan500} />}
+              />
+            </Link>
             <Divider />
           </div>
         ))}
